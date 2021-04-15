5f4879f5c79fdcd9915aa36db1438adaefae0a34
MDCGFernando <marofernando1991@gmail.com>
2021-04-16 00:36:45 +0100

    Testado Classe de Dominio do Projecto

--- src/main/java/co/ao/mfdesenvolvimento/resources/CategoriaResource.java
+++ src/main/java/co/ao/mfdesenvolvimento/resources/CategoriaResource.java
@@ -1,14 +1,24 @@
 package co.ao.mfdesenvolvimento.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.ao.mfdesenvolvimento.domain.Categoria;
 
 @RestController	
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Esta Funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		List<Categoria> lst = new ArrayList<>();
+		lst.add(cat1);
+		lst.add(cat2);
+		return lst;
 	}
 }
