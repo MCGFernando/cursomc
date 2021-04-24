055b48fd900e44be3688180687c3c026139ec4ee
MDCGFernando <marofernando1991@gmail.com>
2021-04-24 23:31:02 +0100

    Criado repository e service para Categoria

--- src/main/java/co/ao/mfdesenvolvimento/resources/CategoriaResource.java
+++ src/main/java/co/ao/mfdesenvolvimento/resources/CategoriaResource.java
@@ -3,22 +3,32 @@ package co.ao.mfdesenvolvimento.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.ao.mfdesenvolvimento.domain.Categoria;
+import co.ao.mfdesenvolvimento.services.CategoriaService;
 
 @RestController	
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informatica");
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+		/*Categoria cat1 = new Categoria(1, "Informatica");
 		Categoria cat2 = new Categoria(2, "Escritorio");
 		List<Categoria> lst = new ArrayList<>();
 		lst.add(cat1);
 		lst.add(cat2);
-		return lst;
+		return lst;*/
 	}
 }
