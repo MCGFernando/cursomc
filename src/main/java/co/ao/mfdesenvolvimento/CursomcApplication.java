ab5df140a89193d13b79d5770db11c657c2289da
MDCGFernando <marofernando1991@gmail.com>
2021-04-27 21:29:43 +0100

    Criado Estad e Cidade

--- src/main/java/co/ao/mfdesenvolvimento/CursomcApplication.java
+++ src/main/java/co/ao/mfdesenvolvimento/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import co.ao.mfdesenvolvimento.domain.Categoria;
+import co.ao.mfdesenvolvimento.domain.Cidade;
+import co.ao.mfdesenvolvimento.domain.Estado;
 import co.ao.mfdesenvolvimento.domain.Producto;
 import co.ao.mfdesenvolvimento.repositories.CategoriaRepository;
+import co.ao.mfdesenvolvimento.repositories.CidadeRepository;
+import co.ao.mfdesenvolvimento.repositories.EstadoRepository;
 import co.ao.mfdesenvolvimento.repositories.ProductoRepository;
 
 @SpringBootApplication
@@ -18,6 +22,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProductoRepository productoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -39,8 +47,24 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
+		
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		productoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est1.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
