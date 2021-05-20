75962948c1ef4cf29e45ce966d88c00b8c8a4f37
MDCGFernando <marofernando1991@gmail.com>
2021-05-20 21:58:53 +0100

    Listando Categorias com FindAll e DTO

--- src/main/java/co/ao/mfdesenvolvimento/resources/CategoriaResource.java
+++ src/main/java/co/ao/mfdesenvolvimento/resources/CategoriaResource.java
@@ -4,6 +4,9 @@ package co.ao.mfdesenvolvimento.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import co.ao.mfdesenvolvimento.domain.Categoria;
+import co.ao.mfdesenvolvimento.dto.CategoriaDTO;
 import co.ao.mfdesenvolvimento.services.CategoriaService;
 
 @RestController	
@@ -48,6 +52,14 @@ public class CategoriaResource {
 	public ResponseEntity<?> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+
 	}
 	/*@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar(){
