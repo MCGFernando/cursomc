3f8deaa79f7d49c36414ff0746f88678aa45d5ae
MDCGFernando <marofernando1991@gmail.com>
2021-06-08 22:53:27 +0100

    Validacao sintatica com bean validation

--- src/main/java/co/ao/mfdesenvolvimento/dto/CategoriaDTO.java
+++ src/main/java/co/ao/mfdesenvolvimento/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package co.ao.mfdesenvolvimento.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import co.ao.mfdesenvolvimento.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
@@ -10,6 +14,10 @@ public class CategoriaDTO implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	//@NotEmpty(message = "Preenchimento obrigatorio")
+	//@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 r 80 caracters")
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 r 80 caracters")
 	private String nome;
 	public CategoriaDTO() {
 	}
--- src/main/java/co/ao/mfdesenvolvimento/services/CategoriaService.java
+++ src/main/java/co/ao/mfdesenvolvimento/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import co.ao.mfdesenvolvimento.domain.Categoria;
+import co.ao.mfdesenvolvimento.dto.CategoriaDTO;
 import co.ao.mfdesenvolvimento.repositories.CategoriaRepository;
 import co.ao.mfdesenvolvimento.services.exceptions.DataIntegrityException;
 import co.ao.mfdesenvolvimento.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String direction, String orderBy){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
